8543ab9de6f2f199e20efb9cc9a37ca1f26e2851
Ryan Eisenbarth <ryan.eisenbarth@gmail.com>
2019-02-27 11:13:13 -0500

    Update ftp_client.c
    run server with gcc server -o server
    takes in command ./server <PortNumber>

--- ftp_client.c
+++ ftp_client.c
@@ -1,169 +1,87 @@
+#include <stdio.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <stdio.h>
+#include <netdb.h>
 #include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <ctype.h>
 
-int main(int argc, char** argv)
+void error(char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char *argv[])
 {
 
-	char portNum[16]; //port number
-	char ipNum[16];   // ip number
+/*
+ * TODO: Implement LIST, RETRIEVE, STORE, MULTITHREADING
+ * Also fix the warnings
+ */
 
-	printf("Enter port number: "); //asks user for port number
-	fgets(portNum, 16, stdin);
-	
-	// Checking port number size and argument
-	if((atoi(portNum) > 65535) || (atoi(portNum) < 1) ){
-		printf("Invalid port number.\n");
-		return 1;
-	}
-	
-	printf("Enter an IP number: "); // asks user to enter IP number
-	fgets(ipNum, 16, stdin);
-	
-	
-	int counter = 0;
-	char *temp = malloc(16);
-	char *tempIP = malloc(16);
-	strcpy(tempIP, ipNum); //copies the IP number entered
-	temp = strtok(tempIP, "."); // sets the ip number in proper format
 
-	/**
-	 * The temp variable will become null when the last token is 
-	 * read or found.
-	 */
-	while(temp != NULL)
-	{
-		counter++;
-		int i = 0;
-		
-		// checks for valid numbers in ip address
-		while(i < strlen(temp) - 1)
-		{
-			// makes sure section is a number
-			if (isdigit(temp[i])){  
-				i++;
-			}
+    int sockfd, portno, n;
 
-			// if the section of the IP address is not a number
-			else{
-				printf("Not a valid IP Number.\n");
-				return 1;
-			}
-		}
-		// Each section should have a number greater than 0 and less than 255
-		if((atoi(temp)) > 255 || atoi(temp) < 0){
-			printf("Not a valid IP Number.\n");
-			return 1;
-		}
-		temp = strtok(NULL, "."); // adds a dot for the section
-	}
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    char buffer[256];
+    char* quit;
+    //bzero(quit, 256);
+    quit = "QUIT\n";
 
-	// Should be 4 sections of numbers in an IP number.
-	if (counter != 4){
-		printf("Not a valid IP Number. Wrong number of sections.\n");
 
-		free(temp);  // frees the allocated memory
-		free(tempIP);
-		return 1;
-	}
+    /*
+     * Establishing Socket connection
+     */
+    if (argc < 3) {
+        fprintf(stderr,"usage %s hostname port\n", argv[0]);
+        exit(0);
+    }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,
+          (char *)&serv_addr.sin_addr.s_addr,
+          server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+        error("ERROR connecting");
 
-	else{
-		free(tempIP); //frees memory
-		free(temp);
-	}
-	// Creating a socket
-	int sockfd = socket(AF_INET,SOCK_STREAM,0);
+    /*
+     * Loop to keep entering commands
+     */
+    while(1) {
 
-	// error check for creating socket
-	if(sockfd < 0){
-		printf("Error. Could not create socket.\n");
-		return 1;
-	}
+        //writing to server
+        printf("Please enter the message: ");
+        bzero(buffer, 256);
+        fgets(buffer, 255, stdin);
+        n = write(sockfd, buffer, strlen(buffer));
+        if (n < 0)
+            error("ERROR writing to socket");
 
-	// Setting up the socket address
-	struct sockaddr_in serveraddr;
-	serveraddr.sin_family=AF_INET;
-	serveraddr.sin_port=htons(atoi(portNum));
-	serveraddr.sin_addr.s_addr=inet_addr(ipNum);	
+        //breaking out of while loop
+        if(strcmp(buffer, quit) == 0){
+            printf("Closing Connection\n");
+            break;
+        }
 
-	int e = connect(sockfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+        //reading from server
+        bzero(buffer, 256);
+        n = read(sockfd, buffer, 255);
+        if (n < 0)
+            error("ERROR reading from socket");
+        printf("%s\n", buffer);
 
-	/* Check for errors with socket address*/
-	if(e < 0){
-		printf("There was an error with connecting\n");
-		return 1;
-	}
-	// Buffers used in the loop
-	char *line = malloc(5000);
-	char *bufferSize = malloc(sizeof(char) * 10);
-	char *buffer = malloc(sizeof(char) * 512);
-	char *okBuffer = malloc(sizeof(char)* 1);
-	
-	while(1){
-		
-		printf("Enter the file path, or exit: ");
-		fgets(line, 5000, stdin);
-		if(strncmp(line, "exit", 4) == 0){
-			printf("Leaving server.\n");
-			send(sockfd, line, strlen(line), 0);
-
-			// free allocated memory
-			free(line);
-			free(bufferSize);
-			free(buffer);
-			free(okBuffer);
-
-			close(sockfd); //close socket
-			return 0;
-		}
-
-		//Eliminating new line from end of input - makes finding file easier on server
-		line = strtok(line, "\n");
-	
-		// Sending file path over socket
-		send(sockfd, line, strlen(line), 0);
-		
-		// Buffer for response if file found on server
-		// Check if file exists before creating anything
-		recv(sockfd, okBuffer, 1, 0);
-		if(strcmp(okBuffer, "1") == 0){
-			printf("File not found\n");
-			continue;
-		}
-	
-	
-		// Getting size of file from server
-		int size = 0;
-		int test = recv(sockfd, bufferSize, 10, 0);
-
-		if(test < 0){
-			printf("Error getting size.\n");
-			close(sockfd);
-			return 1;
-		}
-		size = atoi(bufferSize);
-
-		// File 
-		FILE *file = fopen(line, "w");
-		
-		//Need to receive in loop, as server will send in packets (either implicitly or explicitly)
-		int read;
-		int bytes = size;
-		while(bytes > 0)
-		{
-			read = recv(sockfd, buffer, 512, 0);
-
-			// Only write what was received, not extra bytes.
-			fwrite(buffer, sizeof(char), read, file);
-			bytes -=  read;
-	
-		}
-		printf("Done copying file.\n");
-	}
-	return 0;
-}                                                                                                                                                        169       1,1           Top
+    }
+    return 0;
+}
