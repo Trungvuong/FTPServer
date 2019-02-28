782e6a8aae74cb45420b1ff32c17b88eeee83747
Ryan Eisenbarth <ryan.eisenbarth@gmail.com>
2019-02-28 09:09:19 -0500

    Update ftp_client.c

--- ftp_client.c
+++ ftp_client.c
@@ -5,64 +5,47 @@
 #include <netdb.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-void error(char *msg)
-{
-    perror(msg);
-    exit(0);
-}
+//helper functions
+void error(char *msg);
+int connectPort(char* sName, char* pNum);
 
-int main(int argc, char *argv[])
+int sockfd, portno, n;
+struct sockaddr_in serv_addr;
+struct hostent *server;
+char buffer[256];
+char* quit = "QUIT\n";
+
+int main()
 {
 
 /*
  * TODO: Implement LIST, RETRIEVE, STORE, MULTITHREADING
  * Also fix the warnings
  */
-
-
-    int sockfd, portno, n;
-
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-    char buffer[256];
-    char* quit;
-    //bzero(quit, 256);
-    quit = "QUIT\n";
-
-
-    /*
-     * Establishing Socket connection
-     */
-    if (argc < 3) {
-        fprintf(stderr,"usage %s hostname port\n", argv[0]);
-        exit(0);
-    }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-        error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr,
-          (char *)&serv_addr.sin_addr.s_addr,
-          server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
-        error("ERROR connecting");
-
     /*
      * Loop to keep entering commands
      */
+    char* connectInput;
+    char* firstParam;
+    char* secondParam;
+    printf("Please Connect to Port: ");
+    fgets(connectInput, 256, stdin);
+    strtok(connectInput, " ");
+    firstParam  = strtok(NULL, " ");
+    secondParam = strtok(NULL, " ");
+
+
+    if (connectPort(firstParam, secondParam) == 0){
+        printf("Connected Successfully!\n");
+    }
+
+
     while(1) {
 
         //writing to server
-        printf("Please enter the message: ");
+        printf("Please Enter A Command: ");
         bzero(buffer, 256);
         fgets(buffer, 255, stdin);
         n = write(sockfd, buffer, strlen(buffer));
@@ -77,11 +60,48 @@ int main(int argc, char *argv[])
 
         //reading from server
         bzero(buffer, 256);
-        n = read(sockfd, buffer, 255);
-        if (n < 0)
-            error("ERROR reading from socket");
-        printf("%s\n", buffer);
+        //while(1) {
+            n = read(sockfd, buffer, sizeof(buffer));
+            //if(n == 0){
+                //break;
+           // }
+            if (n < 0)
+                error("ERROR reading from socket");
+            printf("%s\n", buffer);
+
+        //}
 
     }
     return 0;
 }
+
+void error(char *msg){
+perror(msg);
+exit(0);
+}
+
+int connectPort(char* serverName, char* portNum){
+
+    printf("Inside connectPort: %s : %s \n", serverName, portNum);
+    portno = atoi(portNum);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    server = gethostbyname(serverName);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,
+          (char *)&serv_addr.sin_addr.s_addr,
+          server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+        error("ERROR connecting");
+
+    return 0;
+}
+
+
